beccb3f19fddfd77304c20583ca4c338ce6c7aeb
silva-danilo05 <20112943@setu.ie>
2026-03-24 12:28:36 +0000

    add helpers

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -19,14 +19,19 @@ const dashboard = {
   },
 
   addPlaylist(request, response) {
-    const newPlayList = {
+    const timestamp = new Date();
+    
+    const newPlaylist = {
       id: uuidv4(),
       title: request.body.title,
-      songs: [],
+      rating: request.body.rating,
+	    date: timestamp,
+      songs: []
     };
-    playlistStore.addPlaylist(newPlayList);
+    playlistStore.addPlaylist(newPlaylist);
     response.redirect('/dashboard');
   },
+
 
   deletePlaylist(request, response) {
       const playlistId = request.params.id;
