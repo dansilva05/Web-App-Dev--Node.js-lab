4786639b0d91bd4499243a060d541719ed1a141e
silva-danilo05 <20112943@setu.ie>
2026-04-22 03:46:24 +0100

    App statistics added

--- controllers/about.js
+++ controllers/about.js
@@ -1,21 +1,24 @@
 import logger from "../utils/logger.js";
 import employeeStore from "../models/employee-store.js"
+import accounts from './accounts.js';
 
 'use Strict';
 
 const about = {
   createView(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
     logger.info("About page loading!");
-
-    const viewData = {
-      title: "Playlist App About",
-      employees: employeeStore.getAppInfo(),
-    };
-
-    logger.debug(viewData.employees);
-
-    response.render('about', viewData);   
-  },
+    
+    if (loggedInUser) {
+      const viewData = {
+        title: 'About the Playlist App',
+        fullname: loggedInUser.firstName + ' ' + loggedInUser.lastName,
+        employees: empStore.getEmployees(),
+      };
+      response.render('about', viewData);
+    }
+    else response.redirect('/');    
+},
 };
 
 export default about;
--- models/playlist-store.js
+++ models/playlist-store.js
@@ -37,7 +37,17 @@ searchPlaylist(search) {
     return this.store.findBy(
       this.collection,
       (playlist => playlist.title.toLowerCase().includes(search.toLowerCase())))
-}
+},
+
+getUserPlaylists(userid) {
+  return this.store.findBy(this.collection, (playlist => playlist.userid === userid));
+},
+
+searchUserPlaylists(search, userid) {
+  return this.store.findBy(
+    this.collection,
+    (playlist => playlist.userid === userid && playlist.title.toLowerCase().includes(search.toLowerCase())))
+}, 
 };
 
 export default playlistStore;
